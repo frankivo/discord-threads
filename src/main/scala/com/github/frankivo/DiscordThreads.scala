0a2a3f3dcff4cdb0e7022c1df8c9792937bef7c3
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-08-30 11:32:33 +0200

    Connect to discord
    Threads unsupported untill 3.3.0

--- src/main/scala/com/github/frankivo/DiscordThreads.scala
+++ src/main/scala/com/github/frankivo/DiscordThreads.scala
@@ -1,7 +1,40 @@
 package com.github.frankivo
 
+import discord4j.core.DiscordClient
+import discord4j.core.`object`.entity.channel.TextChannel
+import discord4j.core.event.domain.message.MessageCreateEvent
+
 object DiscordThreads {
+  val TOKEN: String = sys.env("discord4j.token")
+
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
+
+    val discord = new DiscordThreads
+    discord.connect()
+  }
+}
+
+class DiscordThreads {
+  def connect(): Unit = {
+    val gateway = DiscordClient
+      .create(DiscordThreads.TOKEN)
+      .login()
+      .block()
+
+    gateway.on(classOf[MessageCreateEvent])
+      .subscribe(e => handle(e))
+
+    gateway.onDisconnect.block
+  }
+
+  def handle(event: MessageCreateEvent): Unit = {
+    event
+      .getMessage
+      .getChannel
+      .block()
+      .asInstanceOf[TextChannel]
+      .createMessage("test!")
+      .subscribe()
   }
 }
